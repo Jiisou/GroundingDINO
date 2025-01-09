18946006f30ba1c7fe7bac3540a1eae4652ee2b1
jisu <wltnslagg@naver.com>
2025-01-09 07:27:10 +0000

    bug fix: PIL 이미지가 ndarray처리불가 오류 해결, 텐서로 변환후 입력

--- video_inference.py
+++ video_inference.py
@@ -1,6 +1,11 @@
 import cv2
 from groundingdino.util.inference import load_model, load_image, predict, annotate
 import os
+from PIL import Image
+from torchvision import transforms
+import torch
+from tqdm import tqdm
+
 
 # 모델 로딩
 model = load_model("groundingdino/config/GroundingDINO_SwinT_OGC.py", "/home/work/GroundingDINO/weights/groundingdino_swint_ogc.pth")
@@ -25,11 +30,22 @@ while True:
     if not ret:
         break  # 비디오 끝
 
-    # 프레임 전처리 및 객체 탐지 수행
-    image_source, image = load_image(frame)
+    # numpy.ndarray를 PIL 이미지로 변환
+    image_source = cv2.cvtColor(frame, cv2.COLOR_BGR2RGB)  # OpenCV에서 BGR을 RGB로 변환
+    image_pil = Image.fromarray(image_source)  # numpy.ndarray에서 PIL.Image로 변환
+
+    # PIL 이미지를 Torch Tensor로 변환
+    transform = transforms.ToTensor()  # ToTensor 변환 객체 생성
+    image_tensor = transform(image_pil)  # PIL.Image를 Torch Tensor로 변환
+
+    # image_tensor는 이제 Torch Tensor 형태로, device로 이동 가능합니다.
+    device = "cuda" if torch.cuda.is_available() else "cpu"
+    image_tensor = image_tensor.to(device)
+
+    # 객체 탐지 수행
     boxes, logits, phrases = predict(
         model=model,
-        image=image,
+        image=image_tensor,
         caption=TEXT_PROMPT,
         box_threshold=BOX_TRESHOLD,
         text_threshold=TEXT_TRESHOLD
