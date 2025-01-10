eae2626e462e414ce9578ff6e77211365ca3c1e0
jisu <wltnslagg@naver.com>
2025-01-10 00:55:22 +0000

    feature: bbox custom

--- groundingdino/util/inference.py
+++ groundingdino/util/inference.py
@@ -121,8 +121,14 @@ def annotate(image_source: np.ndarray, boxes: torch.Tensor, logits: torch.Tensor
         in zip(phrases, logits)
     ]
 
+    # 박스 설정 
     bbox_annotator = sv.BoxAnnotator(color_lookup=sv.ColorLookup.INDEX)
-    label_annotator = sv.LabelAnnotator(color_lookup=sv.ColorLookup.INDEX)
+    # 레이블 설정 조정
+    label_annotator = sv.LabelAnnotator(
+        color_lookup=sv.ColorLookup.INDEX,
+        text_padding=2,   # 텍스트 패딩
+    )
+
     annotated_frame = cv2.cvtColor(image_source, cv2.COLOR_RGB2BGR)
     annotated_frame = bbox_annotator.annotate(scene=annotated_frame, detections=detections)
     annotated_frame = label_annotator.annotate(scene=annotated_frame, detections=detections, labels=labels)
